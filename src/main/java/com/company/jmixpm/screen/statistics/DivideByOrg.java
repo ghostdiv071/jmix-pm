7266de088df7784cd4d3826f05b7f731ca6ce6a5
aminakov <aminakov@phoenixit.ru>
2022-05-24 12:53:23 +0300

    charts

---
+++ src/main/java/com/company/jmixpm/screen/statistics/DivideByOrg.java
@@ -0,0 +1,12 @@
+package com.company.jmixpm.screen.statistics;
+
+import io.jmix.dashboardsui.annotation.DashboardWidget;
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("DivideByOrg")
+@UiDescriptor("divide-by-org.xml")
+@DashboardWidget("divideByOrgs")
+public class DivideByOrg extends ScreenFragment {
+}
