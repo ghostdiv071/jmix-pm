f341e1bd25d68a3d2813ea8fc877dacb520a348f
aminakov <aminakov@phoenixit.ru>
2022-06-02 16:48:48 +0300

    extended user browse

---
+++ src/main/java/com/company/jmixpm/entity/ExtUser.java
@@ -0,0 +1,30 @@
+package com.company.jmixpm.entity;
+
+import io.jmix.core.metamodel.annotation.DependsOnProperties;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@JmixEntity
+@Entity(name = "ExtUser")
+public class ExtUser extends User {
+    @Column(name = "PHONE_NUMBER", length = 50)
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @InstanceName
+    @DependsOnProperties({"firstName", "lastName", "username"})
+    public String getDisplayName() {
+        return String.format("%s %s [%s]", (firstName != null ? firstName : ""),
+                (lastName != null ? lastName : ""), username).trim();
+    }
+}
---
+++ src/main/java/com/company/jmixpm/screen/extuserbrowse/ExtUserBrowse.java
@@ -0,0 +1,10 @@
+package com.company.jmixpm.screen.extuserbrowse;
+
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+import com.company.jmixpm.screen.user.UserBrowse;
+
+@UiController("ext_User.browse")
+@UiDescriptor("ext-user-browse.xml")
+public class ExtUserBrowse extends UserBrowse {
+}
