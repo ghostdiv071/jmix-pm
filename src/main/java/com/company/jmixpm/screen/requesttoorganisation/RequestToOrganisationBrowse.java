4e5e01569f18e29f73ee84d860fba5482a441fff
aminakov <aminakov@phoenixit.ru>
2022-06-06 14:43:45 +0300

    fix Cancel checkbox

--- src/main/java/com/company/jmixpm/screen/requesttoorganisation/RequestToOrganisationBrowse.java
+++ src/main/java/com/company/jmixpm/screen/requesttoorganisation/RequestToOrganisationBrowse.java
@@ -1,17 +1,24 @@
 package com.company.jmixpm.screen.requesttoorganisation;
 
 import com.company.jmixpm.entity.RequestToOrganisation;
+import io.jmix.core.DataManager;
 import io.jmix.ui.component.Table;
 import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("RequestToOrganisation.browse")
 @UiDescriptor("request-to-organisation-browse.xml")
 @LookupComponent("requestToOrganisationsTable")
 public class RequestToOrganisationBrowse extends StandardLookup<RequestToOrganisation> {
 
+    @Autowired
+    private DataManager manager;
+
     @Subscribe("requestToOrganisationsTable.cancelled")
     public void onRequestToOrganisationsTableCancelledClick(Table.Column.ClickEvent<RequestToOrganisation> event) {
-        event.getItem().setCancelled(true);
+        var entity = event.getItem();
+        entity.setCancelled(true);
+        manager.save(entity);
     }
 
 }
