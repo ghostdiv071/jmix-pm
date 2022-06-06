5c6846a9a3d784992c5977716d478a4158f82454
aminakov <aminakov@phoenixit.ru>
2022-06-06 18:13:05 +0300

    added automatic numbering

--- src/main/java/com/company/jmixpm/screen/requesttoorganisation/RequestToOrganisationEdit.java
+++ src/main/java/com/company/jmixpm/screen/requesttoorganisation/RequestToOrganisationEdit.java
@@ -1,10 +1,22 @@
 package com.company.jmixpm.screen.requesttoorganisation;
 
-import io.jmix.ui.screen.*;
 import com.company.jmixpm.entity.RequestToOrganisation;
+import io.jmix.core.DataManager;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("RequestToOrganisation.edit")
 @UiDescriptor("request-to-organisation-edit.xml")
 @EditedEntityContainer("requestToOrganisationDc")
 public class RequestToOrganisationEdit extends StandardEditor<RequestToOrganisation> {
+    @Autowired
+    private DataManager manager;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<RequestToOrganisation> event) {
+        event.getEntity().setRequestNumber(
+                manager.load(RequestToOrganisation.class)
+                        .all().list().size() + 1);
+    }
+
 }
